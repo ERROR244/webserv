f20337226b88353b1d42d981cc952f018b595e21
ERROR244 <khalilsohail24@gmail.com>
2025-03-06 21:02:45 +0000

    add Abnormal termination Signal of the program `SIGABRT`.

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,29 +1,48 @@
 #include "server.h"
 #include "confiClass.hpp"
+#include <termios.h>
+
+void disableEchoCtrl() {
+    struct termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    tty.c_lflag &= ~ECHOCTL; // Disable printing of control characters (like ^C)
+    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+}
+
+void signalHandler( int signum ) {
+   cout << "Interrupt signal (" << signum << ") received.\n";
+   exit(signum);
+}
+
+void getConfi(char *fileName, map<string, configuration>& config, int& epollFd, vector<int> &serverFds) {
+    try {
+        ConfigFileParser confi(fileName);
+        config = confi.parseFile();
+        // confi.printprint();
+        epollFd = createSockets(config, serverFds);
+    }
+    catch (const exception& e) {
+        cerr << e.what() << endl;
+        exit(-1);
+    }
+    catch (...) {
+        cerr << "ERROR" << endl;
+        exit(-1);
+    }
+}
 
 int main(int ac, char **av) {
+    map<string, configuration> config;
+    vector<int> serverFds;
+    int epollFd;
+    
     if (ac != 2) {
         cerr << "invalid number of argument" << endl;
     } else {
         //config file
-        vector<int> serverFds;
-        int epollFd;
-        map<string, configuration> config;
-        try {
-            ConfigFileParser confi(av[1]);
-            config = confi.parseFile();
-            confi.printprint();
-            //server setuping
-            epollFd = createSockets(config, serverFds);
-        }
-        catch (const std::runtime_error& error) {
-            cerr << error.what() << endl;
-            return -1;
-        }
-        catch (...) {
-            cerr << "ERROR" << endl;
-            return -1;
-        }
+        disableEchoCtrl();
+        signal(SIGINT, signalHandler);
+        getConfi(av[1], config, epollFd, serverFds);       
         //multiplexer
         try {
             multiplexerSytm(serverFds, epollFd, config);
